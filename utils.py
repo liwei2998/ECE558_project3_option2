fafd77c080b67694425d682c6965f5e9ca8fc656
Gordon Chen <61254009+hgv523@users.noreply.github.com>
2022-12-10 23:36:23 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+import logging
 
 class LineAnnotation:
     def init(self,img_bgr,style):
@@ -98,6 +99,8 @@ class helper:
 class LineDetector:
     def init(self,img):
         self.img = cv2.resize(img,(640,480))
+        image = self.img
+        return image
 
     def detect(self,mask_on=True,self_adjust=False):
         # a) Add masks when the image boundry colors are hard to tell
@@ -141,7 +144,7 @@ class LineDetector:
         cv2.waitKey(0)   
         cv2.destroyAllWindows()
 
-        return lines     
+        return lines   
 
     def color_filter(self):
         # This is to get HSV color from a given image, used for color filtering
@@ -180,6 +183,134 @@ class LineDetector:
         result = cv2.cvtColor(frame,cv2.COLOR_HSV2BGR)
         return mask
 
+class VanishingPoint:
+    def compute_edgelets(self,lines, sigma = 3):
+        #compute locations, directions, strengths given detected lines
+        #locations: ndarray of shape (n_edgelets, 2)
+        #    Locations of each of the edgelets.
+        #directions: ndarray of shape (n_edgelets, 2)
+        #    Direction of the edge (tangent) at each of the edgelet.
+        #strengths: ndarray of shape (n_edgelets,)
+        #    Length of the line segments detected for the edgelet.
+        locations = []
+        directions = []
+        strengths = []
+        for p0x,p0y,p1x,p1y in lines:
+            p0, p1 = np.array([p0x, p0y]), np.array([p1x, p1y])
+            locations.append((p0 + p1) / 2)
+            directions.append(p1 - p0)
+            strengths.append(np.linalg.norm(p1 - p0))
+        # convert to numpy arrays and normalize
+        locations = np.array(locations)
+        directions = np.array(directions)
+        strengths = np.array(strengths)
+        directions = np.array(directions) / \
+            np.linalg.norm(directions, axis=1)[:, np.newaxis]
+        return (locations, directions, strengths)
+
+    def edgelet_lines(self, edgelets):
+        #compute lines in homogenous system for edglets.
+        locations, directions, _ = edgelets
+        normals = np.zeros_like(directions)
+        normals[:, 0] = directions[:, 1]
+        normals[:, 1] = -directions[:, 0]
+        p = -np.sum(locations * normals, axis=1)
+        lines = np.concatenate((normals, p[:, np.newaxis]), axis=1)
+        return lines
+
+    def compute_votes(self, edgelets, model, threshold_inlier=5):
+        #compute votes for each of the edgelet against a given vanishing point
+        #votes for edgelets which lie inside threshold are same as their strengths, otherwise zero
+        vp = model[:2] / model[2]
+        locations, directions, strengths = edgelets
+        est_directions = locations - vp
+        dot_prod = np.sum(est_directions * directions, axis=1)
+        abs_prod = np.linalg.norm(directions, axis=1) * \
+            np.linalg.norm(est_directions, axis=1)
+        abs_prod[abs_prod == 0] = 1e-5
+        cosine_theta = dot_prod / abs_prod
+        theta = np.arccos(np.abs(cosine_theta))
+        theta_thresh = threshold_inlier * np.pi / 180
+        return (theta < theta_thresh) * strengths
+
+    def ransac_vanishing_point(self, edgelets, num_ransac_iter=2000, threshold_inlier=5):
+        #estimate vanishing point using Ransac
+        locations, directions, strengths = edgelets
+        lines = self.edgelet_lines(edgelets)
+        num_pts = strengths.size
+        arg_sort = np.argsort(-strengths)
+        first_index_space = arg_sort[:num_pts // 5]
+        second_index_space = arg_sort[:num_pts // 2]
+        best_model = None
+        best_votes = np.zeros(num_pts)
+        for ransac_iter in range(num_ransac_iter):
+            ind1 = np.random.choice(first_index_space)
+            ind2 = np.random.choice(second_index_space)
+            l1 = lines[ind1]
+            l2 = lines[ind2]
+            current_model = np.cross(l1, l2)
+            if np.sum(current_model**2) < 1 or current_model[2] == 0:
+                # reject degenerate candidates
+                continue
+            current_votes = self.compute_votes(
+                edgelets, current_model, threshold_inlier)
+
+            if current_votes.sum() > best_votes.sum():
+                best_model = current_model
+                best_votes = current_votes
+                logging.info("Current best model has {} votes at iteration {}".format(
+                    current_votes.sum(), ransac_iter))
+        return best_model
+
+    def reestimate_model(self, model, edgelets, threshold_reestimate=5):
+        #Reestimate vanishing point using inliers and least squares.
+        locations, directions, strengths = edgelets
+        inliers = self.compute_votes(edgelets, model, threshold_reestimate) > 0
+        locations = locations[inliers]
+        directions = directions[inliers]
+        strengths = strengths[inliers]
+        lines = self.edgelet_lines((locations, directions, strengths))
+        a = lines[:, :2]
+        b = -lines[:, 2]
+        est_model = np.linalg.lstsq(a, b)[0]
+        return np.concatenate((est_model, [1.]))
+
+    def vis_edgelets(self, image, edgelets, show=True):
+        #Helper function to visualize edgelets
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        locations, directions, strengths = edgelets
+        for i in range(locations.shape[0]):
+            xax = [locations[i, 0] - directions[i, 0] * strengths[i] / 2,
+                   locations[i, 0] + directions[i, 0] * strengths[i] / 2]
+            yax = [locations[i, 1] - directions[i, 1] * strengths[i] / 2,
+                   locations[i, 1] + directions[i, 1] * strengths[i] / 2]
+
+            plt.plot(xax, yax, 'r-')
+
+        if show:
+            plt.show()
+
+    def vis_model(self, image, edgelets, final_model, show=True):
+        #Helper function to visualize computed model.
+        locations, directions, strengths = edgelets
+        inliers = self.compute_votes(edgelets, final_model, 10) > 0
+
+        edgelets = (locations[inliers], directions[inliers], strengths[inliers])
+        locations, directions, strengths = edgelets
+        self.vis_edgelets(image, edgelets, False)
+        vp = final_model / final_model[2]
+        plt.plot(vp[0], vp[1], 'bo')
+        for i in range(locations.shape[0]):
+            xax = [locations[i, 0], vp[0]]
+            yax = [locations[i, 1], vp[1]]
+            plt.plot(xax, yax, 'b-.')
+
+        if show:
+            plt.show()
+    
+    
 if __name__ == '__main__':
     ############# class testing
     img = cv2.imread('box.jpg')
@@ -187,9 +318,14 @@ if __name__ == '__main__':
 
     ###LineDetector
     lsd = LineDetector()
-    lsd.init(img)
-    lsd.detect(mask_on=False,self_adjust=False) # original image
+    image = lsd.init(img)
+    lines = lsd.detect(mask_on=False,self_adjust=False) # original image
     # lsd.detect(self_adjust=False) # masked image with default mask
     # lsd.detect(self_adjust=True) # masked image with self-adjusted mask
+    vp = VanishingPoint()
+    edgelets = vp.compute_edgelets(lines)
+    best_model = vp.ransac_vanishing_point(edgelets)
+    final_model = vp.reestimate_model(best_model, edgelets)
+    vp.vis_model(image, edgelets, final_model, show = True)
 
 
