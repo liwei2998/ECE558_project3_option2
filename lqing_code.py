08f328e69339467080648b3b579a6a0ec6de6056
liwei2998 <55369454+liwei2998@users.noreply.github.com>
2022-12-11 19:38:02 -0500

    Add files via upload

--- lqing_code.py
+++ lqing_code.py
@@ -7,43 +7,19 @@ os.system('cls')
 if __name__ == '__main__':
     ########Preparation: instantiate helpful classes
     hp = utils.helper()
-    
 
     # Step 1: Image acquisition
     img = cv2.imread('box.jpg')
     
-    # Step 2.1: Image annotation, choose lines either by diy or by default
-    la = utils.LineAnnotation()
-    la.init(img,style='default')
-    lines = la.draw_lines()
-    img=la.draw_image()
-    cv2.imshow('annotated image',img)
-    cv2.waitKey() # show the annotated image, press q to exit 
-    cv2.destroyAllWindows()
-    lines = np.array(lines)
-    lines = lines[[0,3,1,4,2,5],:] # permutation, 0-2: red, 2-4:green, 4-6:blue
-
-    # # Step 2.2: Compute vanishing points
-    # vps = []
-    # for i in range(0,len(lines),2):
-    #     e11 = np.append(lines[i][0],1)
-    #     e12 = np.append(lines[i][1],1)
-    #     l1 = np.cross(e11,e12)
-    #     e21 = np.append(lines[i+1][0],1)
-    #     e22 = np.append(lines[i+1][1],1)
-    #     l2 = np.cross(e21,e22)    
-    #     vp = np.cross(l1,l2)
-    #     vp = vp/vp[2]
-    #     vps.append(vp)
-    # vps = np.array(vps)
+    # Step 2.1: Line segmentation with canny and houghline. Add mask for mcolor filtering. 
+    lsd = utils.LineDetector()
+    image = lsd.init(img)
+    detect_lines = lsd.detect(mask_on=False,self_adjust=False) # original image
+    # detect_lines = lsd.detect(self_adjust=False) # masked image with default mask
+    # detect_lines = lsd.detect(self_adjust=True) # masked image with self-adjusted mask
     
     #Step 2.2: Compute vanishing point using LSD and RANSAC
     vps = []
-    lsd = utils.LineDetector()
-    image = lsd.init(img)
-    #detect_lines = lsd.detect(mask_on=False,self_adjust=False) # original image
-    detect_lines = lsd.detect(self_adjust=False) # masked image with default mask
-    #detect_lines = lsd.detect(self_adjust=True) # masked image with self-adjusted mask
     vp = utils.VanishingPoint()
     edgelets = vp.compute_edgelets(detect_lines)
     model = vp.ransac_vanishing_point(edgelets,20)
@@ -73,21 +49,18 @@ if __name__ == '__main__':
     r2 = r2/np.linalg.norm(r2)
     r1 = np.dot(np.linalg.inv(K),vps[0])
     r1 = r1/np.linalg.norm(r1)
-    #r3 could be computed either by cross r1 & r2, or using the third vanishing point, \
+    # r3 could be computed either by cross r1 & r2, or using the third vanishing point, \
     # be careful about the direction of r3 if using vanishing point
     r3 = np.cross(r1,r2) 
     R = np.array([r1,r2,r3]) # rotation matrix
 
     # c) compute translation matrix
     ow = np.array([0,0,0,1]) #origin in world coordinate
-    # oc = (lines[0][0]+lines[2][0]+lines[4][0])/3 #origin in image coordinate
-    # oc = np.append(oc,1.0)
     oc = np.array([259.3, 406.7, 1])
     t_eq1 = np.dot(np.linalg.inv(K),oc.reshape(3,1)) #t_eq1=[t1/t3,t2/t3,1]
     # The most tricky part, scale pw to make projected picture looks nicer.
     pw = np.array([240,0,0,1]) #point in world coordinate (14.2 is the length of the box)
-    pc = lines[0][1] #corresponding point in image coordinate
-    pc = np.append(pc,1.)
+    pc = np.array([451.,321.,1.]) #corresponding point in image coordinate
     # t_eq2=[(14.2*r11+t1)/(14.2*r13+t3),(14.2*r12+t2)/(14.2*r13+t3),1]
     t_eq2 = np.dot(np.linalg.inv(K),pc.reshape(3,1)) 
     # Simple (trival) math to solve t1,t2,t3 using the two equations about t
